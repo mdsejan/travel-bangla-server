aa76a9bfe8c5c01ee631024b70dc33e7610cdd41
Sejan~ <md4sejan@gmail.com>
2023-11-26 15:30:48 +0600

    connect to mongodb and package get api created

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
+require('dotenv').config();
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // MiddleWare
@@ -8,6 +10,36 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//DB URI
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0viwxwm.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const packageCollection = client.db('travelBangla').collection('packages');
+
+        // packages Related API
+        app.get('/api/v1/packages', async (req, res) => {
+            const result = await packageCollection.find().toArray();
+            res.send(result);
+        })
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('TravelBangla server is running')
